0b55a160aa8d6f3a4e0da675ec58ff47be025a8d
Rafael Minguet <rminguet@gmail.com>
2017-11-09 19:24:24 +0100

    Day 2017-11-07

--- tests/my-component.test.js
+++ tests/my-component.test.js
@@ -1,26 +1,35 @@
 import test from 'tape';
 
+const Income = function (incomeDate, incomeCredit) {
+    return {incomeDate, incomeCredit}
+};
+
 // Component to test
 const BankAccount = function () {
     const incomes = [];
 
     const updateIncome = function (incomeDate, incomeCredit) {
-        incomes.push({incomeDate, incomeCredit});
+        const income = Income(incomeDate, incomeCredit);
+        incomes.push(income);
         return this;
     };
 
     const getBalance = function () {
-        let balance = null;
+        let incomeLines = [];
+        let balance = 0;
         incomes.forEach((val) => {
-            balance = {date: val.incomeDate, credit: val.incomeCredit, balance: val.incomeCredit};
+            balance += val.incomeCredit;
+            incomeLines.push({date: val.incomeDate, credit: val.incomeCredit, balance});
         });
-        return [balance];
+        return incomeLines;
     };
 
     const toString = function () {
         let detailedBalance = 'date || credit || balance\r\n';
+        let balance = 0;
         incomes.forEach((val) => {
-            detailedBalance += `${val.incomeDate} || ${val.incomeCredit} || ${val.incomeCredit}\r\n`;
+            balance += val.incomeCredit;
+            detailedBalance += `${val.incomeDate} || ${val.incomeCredit} || ${balance}\r\n`;
         });
 
         return detailedBalance;
@@ -32,11 +41,30 @@ const BankAccount = function () {
 // TESTS
 test('-------- Retrieving One Income ', (assert) => {
     const message = 'The Balance is five and the first Income is five.';
-    const expected = [{date: '02/04/2014', credit: '5', balance: '5'}];
+    const expected = [{date: '02/04/2014', credit: 5, balance: 5}];
 
     const bankAccount = BankAccount();
-    bankAccount.updateIncome('02/04/2014', '5');
-    const actual = bankAccount.getBalance();
+    const actual = bankAccount
+        .updateIncome('02/04/2014', 5)
+        .getBalance();
+
+    assert.deepEqual(actual, expected, message);
+
+    assert.end();
+});
+
+test('-------- Retrieving Two Income ', (assert) => {
+    const message = 'The Balance is fifteen and we have two incomes of five and ten units.';
+    const expected = [
+        {date: '02/04/2014', credit: 5, balance: 5},
+        {date: '10/04/2014', credit: 10, balance: 15}
+    ];
+
+    const bankAccount = BankAccount();
+    const actual = bankAccount
+        .updateIncome('02/04/2014', 5)
+        .updateIncome('10/04/2014', 10)
+        .getBalance();
 
     assert.deepEqual(actual, expected, message);
 
@@ -48,7 +76,23 @@ test('-------- Retrieving One Income (in text)', (assert) => {
     const expected = 'date || credit || balance\r\n02/04/2014 || 5 || 5\r\n';
 
     const bankAccount = BankAccount();
-    const actual = bankAccount.updateIncome('02/04/2014', '5').toString();
+    const actual = bankAccount.updateIncome('02/04/2014', 5).toString();
+
+    assert.equal(actual, expected, message);
+
+    assert.end();
+});
+
+test('-------- Retrieving Two Income (in text)', (assert) => {
+    const message = 'Returned value is in formatted text';
+    const expected = 'date || credit || balance\r\n02/04/2014 || 5 || 5\r\n10/04/2014 || 10 || 15\r\n';
+
+    const bankAccount = BankAccount();
+    const actual = bankAccount
+        .updateIncome('02/04/2014', 5)
+        .updateIncome('10/04/2014', 10)
+        .toString();
+
 
     assert.equal(actual, expected, message);
 
